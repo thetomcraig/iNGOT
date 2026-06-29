5b8915f364070591433faa93eeb70b46d0d553e1
Tom Craig <thetomcraig@icloud.com>
2026-06-28 20:48:12 -0700

    cleaup

--- software/app.py
+++ software/app.py
@@ -1,9 +1,8 @@
 import requests
-from flask import Flask, jsonify, render_template, request, url_for
+from flask import Flask, jsonify, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 
-# HA_URL = "http://127.0.0.1:8123"
 HA_URL = "http://ariston:8123"
 ENV_VARS = {}
 
@@ -21,7 +20,6 @@ def call_service(domain, service, payload):
     url = f"{HA_URL}/api/services/{domain}/{service}"
     return requests.post(url, headers=HEADERS, json=payload)
 
-from flask import redirect
 
 
 @app.route("/living_room_toggle", methods=['GET', 'POST'])
@@ -31,7 +29,6 @@ def living_room_toggle():
         "toggle",
         {"entity_id": "switch.living_room_light"}
     )
-    return redirect(f"{request.referrer or url_for('index')}")
 
 @app.route("/dining_room_toggle", methods=['GET', 'POST'])
 def dining_room_toggle():
@@ -40,7 +37,8 @@ def dining_room_toggle():
         "toggle",
         {"entity_id": "light.dining_room_light"}
     )
-    return redirect(f"{request.referrer or url_for('index')}")
+    # prob dont need this
+    # return redirect(f"{request.referrer or url_for('index')}")
 
 @app.route("/downstairs_toggle", methods=['GET', 'POST'])
 def downstairs_toggle():
@@ -49,11 +47,10 @@ def downstairs_toggle():
         "toggle_downstairs_lights",
         {}
     )
-    return redirect(f"{request.referrer or url_for('index')}")
+
 @app.route("/playpause")
 def playpause():
     call_service("media_player", "media_play_pause", {"entity_id": "media_player.tv"})
-    return redirect(f"{request.referrer or url_for('index')}")
 
 @app.route("/living_room")
 def index():
