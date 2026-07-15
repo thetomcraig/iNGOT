e4c9b1cf156e5d33a4e6fad8534e21d5c8c7e6b3
Tom Craig <thetomcraig@icloud.com>
2026-07-14 19:25:12 -0700

    move weather to js file

---
+++ software/flask_app/static/weather.js
@@ -0,0 +1,61 @@
+(function () {
+  function setupWeather() {
+  var tempTiles = [
+    {
+      number: document.getElementById("eloises-temp-number"),
+      url: "/eloises_temp"
+    },
+    {
+      number: document.getElementById("outside-temp-number"),
+      url: "/outside_temp"
+    }
+  ];
+
+  var refreshIntervalMs = 10 * 60 * 1000;
+  var tempRequests = {};
+
+  function refreshTemp(tile) {
+    var tempRequest = tempRequests[tile.url];
+
+    if (tempRequest && tempRequest.readyState !== 4) {
+      return;
+    }
+
+    tempRequest = new XMLHttpRequest();
+    tempRequests[tile.url] = tempRequest;
+    tempRequest.onreadystatechange = function () {
+      if (tempRequest.readyState === 4 && tempRequest.status === 200) {
+        tile.number.textContent = tempRequest.responseText.replace("°", "").trim();
+      }
+    };
+    tempRequest.open("GET", tile.url + "?t=" + Date.now(), true);
+    tempRequest.send();
+  }
+
+  function refreshTemps() {
+    tempTiles.forEach(refreshTemp);
+  }
+
+  refreshTemps();
+  window.setInterval(refreshTemps, refreshIntervalMs);
+
+  var refreshPage = document.getElementById("refresh-page");
+  if (refreshPage) {
+    refreshPage.addEventListener("click", function () {
+      window.setTimeout(function () {
+        window.location.reload();
+      }, 180);
+    });
+  }
+}
+
+
+  if (window.addEventListener) {
+    window.addEventListener("load", setupWeather, false);
+  } else if (window.attachEvent) {
+    window.attachEvent("onload", setupWeather);
+  } else {
+    window.onload = setupWeather;
+  }
+
+})();
