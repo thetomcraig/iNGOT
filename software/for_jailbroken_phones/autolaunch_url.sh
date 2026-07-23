dc4f0e8feb6e75c3972f694a1c4dded4ffffbcc6
Tom Craig <thetomcraig@icloud.com>
2026-07-23 10:21:58 -0700

    autolaunch script testing

--- software/for_jailbroken_phones/autolaunch_url.sh
+++ software/for_jailbroken_phones/autolaunch_url.sh
@@ -2,7 +2,7 @@
 
 killall MobileSafari
 
-uiopen "http://192.168.1.131:5001/living_room_960x640"
+uiopen "http://192.168.1.131:5001/office_1136x640"
 
 until uiopen "calshow:" 2>/dev/null; do
     sleep 2
@@ -10,4 +10,4 @@ done
 
 sleep 5
 
-uiopen "http://192.168.1.131:5001/living_room_960x640"
+uiopen "http://192.168.1.131:5001/office_1136x640"
