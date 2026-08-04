a05576f98666c22cb619b25dc249a0a58f7dba28
Tom Craig <thetomcraig@icloud.com>
2026-08-04 15:30:43 -0700

    add older browser compatible bordering and more lamp states

--- software/flask_app/static/scripts/home_assistant_state_manager.js
+++ software/flask_app/static/scripts/home_assistant_state_manager.js
@@ -4,7 +4,8 @@
     "switch.3d_printer": ".three-d-printer",
     "switch.office_lamp": ".office-lamp",
     "light.office_ceiling_light_north": ".office-ceiling-lights",
-    "light.office_fan_light_west": ".office-fan-lights"
+    "light.office_fan_light_west": ".office-fan-lights",
+    "switch.eloise_s_lamp": ".eloise-s-lamp",
   };
 
   function applyHomeAssistantStates(homeAssistantStates) {
@@ -13,9 +14,14 @@
       var isOn = entityState && entityState.state === "on";
       var elements = document.querySelectorAll(stateElementMap[entityId]);
 
-      elements.forEach(function (element) {
-        element.classList.toggle("is-on", isOn);
-      });
+      // iOS 8 lacks NodeList.forEach and classList.toggle's force argument.
+      for (var i = 0; i < elements.length; i += 1) {
+        if (isOn) {
+          elements[i].classList.add("is-on");
+        } else {
+          elements[i].classList.remove("is-on");
+        }
+      }
     });
   }
 
