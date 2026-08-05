0760de866a5b5a28ac8b310be3d04a92a0c18042
Tom Craig <thetomcraig@icloud.com>
2026-08-04 21:50:22 -0700

    refresh page every 10 minutes

--- software/flask_app/static/scripts/page_refresh.js
+++ software/flask_app/static/scripts/page_refresh.js
@@ -1,29 +1,7 @@
 (function () {
-  function refreshAfterAction() {
-    var sink = document.querySelector('iframe[name="sink"]');
-    var refreshPending = false;
+  var refreshIntervalMs = 10 * 60 * 1000;
 
-    if (!sink) return;
-
-    document.addEventListener("submit", function (event) {
-      if (event.target.getAttribute("target") === "sink") {
-        refreshPending = true;
-      }
-    });
-
-    sink.addEventListener("load", function () {
-      if (!refreshPending) return;
-
-      refreshPending = false;
-      window.location.reload();
-    });
-  }
-
-  if (window.addEventListener) {
-    window.addEventListener("load", refreshAfterAction, false);
-  } else if (window.attachEvent) {
-    window.attachEvent("onload", refreshAfterAction);
-  } else {
-    window.onload = refreshAfterAction;
-  }
+  window.setTimeout(function () {
+    window.location.reload();
+  }, refreshIntervalMs);
 }());
