c399b42afc2c8b779a3463b2b4e94c19e0b172d9
Tom Craig <thetomcraig@icloud.com>
2026-08-12 20:49:37 -0700

    simplify api button setup

--- software/flask_app/static/scripts/home_assistant_state_manager.js
+++ software/flask_app/static/scripts/home_assistant_state_manager.js
@@ -1,15 +1,4 @@
 (function () {
-  // Map each Home Assistant entity to the elements that visually represent it.
-  var stateElementMap = {
-    "switch.3d_printer": ".three-d-printer",
-    "switch.office_lamp": ".office-lamp",
-    "switch.libby_s_office_lamp": ".libby-s-office-lamp",
-    "switch.eloise_s_lamp": ".eloise-s-lamp",
-    "light.office_ceiling_light_north": ".office-ceiling-lights",
-    "light.office_fan_light_west": ".office-fan-lights",
-    "switch.living_room_light": ".living-room-light",
-    "light.dining_room_light": ".dining-room-light",
-  };
 
   function hasClass(element, className) {
     return new RegExp("(^|\\s)" + className + "(?:\\s|$)").test(element.className);
@@ -26,29 +15,105 @@
     element.className = element.className.replace(classPattern, " ").replace(/^\\s+|\\s+$/g, "");
   }
 
-  function applyHomeAssistantStates(homeAssistantStates) {
-    var entityId;
+  /**
+   * Read the Home Assistant entity associated with an icon.
+   *
+   * @param {Element} element Icon element to inspect.
+   * @returns {string|null} Associated entity ID, if any.
+   */
+  function entityIdForElement(element) {
+    return element.getAttribute("data-ha-entity");
+  }
 
-    // iOS 3 predates Object.keys and element.classList.
-    for (entityId in stateElementMap) {
-      if (!stateElementMap.hasOwnProperty(entityId)) {
+  /**
+   * Set the visual on/off state for every icon representing an entity.
+   *
+   * @param {string} entityId Home Assistant entity ID.
+   * @param {boolean} isOn Whether the entity is on.
+   */
+  function setEntityOn(entityId, isOn) {
+    var images = document.getElementsByTagName("img");
+    var i;
+
+    for (i = 0; i < images.length; i += 1) {
+      if (entityIdForElement(images[i]) !== entityId) {
         continue;
       }
 
-      var entityState = homeAssistantStates[entityId];
-      var isOn = entityState && entityState.state === "on";
-      var elements = document.querySelectorAll(stateElementMap[entityId]);
+      if (isOn) {
+        addClass(images[i], "is-on");
+      } else {
+        removeClass(images[i], "is-on");
+      }
+    }
+  }
 
-      for (var i = 0; i < elements.length; i += 1) {
-        if (isOn) {
-          addClass(elements[i], "is-on");
-        } else {
-          removeClass(elements[i], "is-on");
-        }
+  /**
+   * Synchronize all stateful icons with a Home Assistant state collection.
+   *
+   * @param {Object} homeAssistantStates States indexed by entity ID.
+   */
+  function applyHomeAssistantStates(homeAssistantStates) {
+    var images = document.getElementsByTagName("img");
+    var updatedEntities = {};
+    var i;
+
+    for (i = 0; i < images.length; i += 1) {
+      var entityId = entityIdForElement(images[i]);
+      if (!entityId || updatedEntities[entityId]) {
+        continue;
+      }
+
+      updatedEntities[entityId] = true;
+      var entityState = homeAssistantStates[entityId];
+      setEntityOn(entityId, entityState && entityState.state === "on");
+    }
+  }
+
+  /**
+   * Optimistically toggle every stateful icon contained by a submitted form.
+   *
+   * @param {HTMLFormElement} form Form whose API action was submitted.
+   */
+  function toggleStateForForm(form) {
+    var images = form.getElementsByTagName("img");
+    var toggledEntities = {};
+    var i;
+
+    for (i = 0; i < images.length; i += 1) {
+      var entityId = entityIdForElement(images[i]);
+      if (!entityId || toggledEntities[entityId]) {
+        continue;
+      }
+
+      toggledEntities[entityId] = true;
+      var entityState = window.homeAssistantStates[entityId] || {};
+      entityState.state = entityState.state === "on" ? "off" : "on";
+      window.homeAssistantStates[entityId] = entityState;
+    }
+
+    // Update every representation of an entity, not just the icon pressed.
+    applyHomeAssistantStates(window.homeAssistantStates);
+  }
+
+  /**
+   * Listen for API form submissions and update their icons immediately.
+   */
+  function attachOptimisticStateToggles() {
+    var forms = document.getElementsByTagName("form");
+    var i;
+
+    for (i = 0; i < forms.length; i += 1) {
+      if (forms[i].addEventListener) {
+        forms[i].addEventListener("submit", function () {
+          toggleStateForForm(this);
+        }, false);
       }
     }
   }
 
   window.applyHomeAssistantStates = applyHomeAssistantStates;
-  applyHomeAssistantStates(window.homeAssistantStates || {});
+  window.homeAssistantStates = window.homeAssistantStates || {};
+  applyHomeAssistantStates(window.homeAssistantStates);
+  attachOptimisticStateToggles();
 }());
