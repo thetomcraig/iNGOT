54bc9b4c93c8d0d1436b69ba4e4e5074ddeea446
Tom Craig <thetomcraig@icloud.com>
2026-08-13 16:31:24 -0700

    gneralize icob script to use hostname

--- software/for_jailbroken_phones/autolaunch_icab.sh
+++ software/for_jailbroken_phones/autolaunch_icab.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 sleep 15
-/usr/bin/uiopen "icabmobile://192.168.1.131:5001/office_1136x640"
+PAGE_NAME=$(scutil --get ComputerName | sed 's/[A-Z]/\L&/g; s/ /_/g')
+/usr/bin/uiopen "icabmobile://192.168.1.131:5001/${PAGE_NAME}"
