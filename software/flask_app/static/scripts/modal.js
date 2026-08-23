684ac1cec0a0c3346250845836ce4244cfebc167
Tom Craig <thetomcraig@icloud.com>
2026-08-22 21:33:03 -0700

    modal cleanup

--- software/flask_app/static/scripts/modal.js
+++ software/flask_app/static/scripts/modal.js
@@ -26,68 +26,31 @@
       backdrop.style.display = "block";
     }
 
-    function clearHold(button) {
-      if (button._holdTimer) {
-        window.clearTimeout(button._holdTimer);
-        button._holdTimer = null;
-      }
-    }
-
-    function startHold(button) {
-      clearHold(button);
-      button._holdTimer = window.setTimeout(function () {
-        button._holdTimer = null;
-        showModal(button.getAttribute("data-modal-open-hold"));
-      }, 1000);
-    }
-
+    // Set up modal buttons
     for (i = 0; i < buttons.length; i++) {
-      if (buttons[i].getAttribute("data-modal-open")) {
-        buttons[i].onclick = function () {
+      var button = buttons[i];
+
+      // Set up modal open button
+      if (button.getAttribute("data-modal-open") !== null) {
+        button.onclick = function(e) {
+          e.preventDefault();
           showModal(this.getAttribute("data-modal-open"));
           return false;
         };
       }
 
-      if (buttons[i].getAttribute("data-modal-open-hold")) {
-        buttons[i].addEventListener("touchstart", function () {
-          this._holdTouch = true;
-          startHold(this);
-        }, false);
-        buttons[i].addEventListener("touchend", function () {
-          clearHold(this);
-          var button = this;
-          window.setTimeout(function () {
-            button._holdTouch = false;
-          }, 500);
-        }, false);
-        buttons[i].addEventListener("touchcancel", function () {
-          clearHold(this);
-          this._holdTouch = false;
-        }, false);
-        buttons[i].addEventListener("mousedown", function () {
-          if (!this._holdTouch) startHold(this);
-        }, false);
-        buttons[i].addEventListener("mouseup", function () {
-          clearHold(this);
-        }, false);
-        buttons[i].addEventListener("mouseleave", function () {
-          clearHold(this);
-        }, false);
-        buttons[i].addEventListener("click", function (event) {
-          clearHold(this);
-          event.preventDefault();
-        }, false);
-      }
-
-      if (buttons[i].getAttribute("data-modal-close") !== null) {
-        buttons[i].onclick = function () {
+      // Handle close button
+      if (button.getAttribute("data-modal-close") !== null && !button._handledClose) {
+        button._handledClose = true;
+        button.addEventListener("click", function(e) {
+          e.preventDefault();
           hideModal();
           return false;
-        };
+        });
       }
     }
 
+    // Handle form submissions in modals
     for (i = 0; i < forms.length; i++) {
       if (forms[i].getAttribute("data-modal-form") !== null) {
         forms[i].onsubmit = function () {
@@ -99,6 +62,7 @@
     backdrop.onclick = hideModal;
   }
 
+  // Run it at load
   if (window.addEventListener) {
     window.addEventListener("load", attachModals, false);
   } else if (window.attachEvent) {
