19011a48d49338965e1207d238234a7dea794510
Tom Craig <thetomcraig@icloud.com>
2026-08-22 20:33:30 -0700

    change refresh to 30 mins

--- software/flask_app/static/scripts/page_refresh.js
+++ software/flask_app/static/scripts/page_refresh.js
@@ -1,5 +1,6 @@
 (function () {
-  var refreshIntervalMs = 10 * 60 * 1000;
+  // var refreshIntervalMs = 10 * 60 * 1000;
+  var refreshIntervalMs = 10 * 60 * 3000;
 
   window.setTimeout(function () {
     window.location.reload();
