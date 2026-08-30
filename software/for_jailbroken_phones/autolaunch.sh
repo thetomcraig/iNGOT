401ee8a09fc8b17526465b784c694e74d152b6da
Tom Craig <thetomcraig@icloud.com>
2026-08-29 21:44:12 -0700

    rename

--- software/for_jailbroken_phones/autolaunch.sh
+++ software/for_jailbroken_phones/autolaunch.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-sleep 15
+sleep 5
+activator send switch-off.com.a3tweaks.switch.rotation-lock
 PAGE_NAME=$(scutil --get ComputerName | sed 's/[A-Z]/\L&/g; s/ /_/g')
 /usr/bin/uiopen "icabmobile://192.168.1.131:5001/${PAGE_NAME}"
+sleep 20
+activator send switch-on.com.a3tweaks.switch.rotation-lock
+
